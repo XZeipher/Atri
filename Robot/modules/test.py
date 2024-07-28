fd0deccc898691124e0fd74485b0fd082dce3efc
XZeipher <144370232+XZeipher@users.noreply.github.com>
2024-07-28 12:37:43 +0530

    Update test.py

--- Robot/modules/test.py
+++ Robot/modules/test.py
@@ -1,5 +1,5 @@
 from pyrogram import *
 
-@Client.on_message(filters.command("me"))
+@Client.on_message(filters.command("me") & filters.me)
 async def test(_,msg):
     return await msg.reply_text("nigga")
