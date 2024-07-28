667cd5d0c2eb3e86ffb28a7e5975b1269bb890e9
XZeipher <144370232+XZeipher@users.noreply.github.com>
2024-07-28 11:23:25 +0530

    Update chatbot.py

--- Robot/modules/chatbot.py
+++ Robot/modules/chatbot.py
@@ -5,12 +5,12 @@ client = Sex()
 token = 'b7a5cf786e6a740638c1b46ad02b035c39d96943'
 
 @Client.on_message(filters.me & filters.text)
-async def chat(client,message):
+async def chat(ctk,message):
     if message.text.lower.startswith("atri"):
         await client.authenticate_with_token(token)
         character_id = "Km_81EXBw_-QSRq1JcAut7jqYUfGhLNcUVO9Ms5xYwQ"
         chat = await client.create_or_continue_chat(character_id)
         message = f"Rishav: {message.text}"
         answer = await chat.send_message(message)
-        return await client.send_message(message.chat.id,f"{answer.src_character_name}: {answer.text}")
+        return await ctk.send_message(message.chat.id,f"{answer.src_character_name}: {answer.text}")
     return
