09aa21ffe2576e4c3ee26d5e72c9ec1f3c8c2101
XZeipher <144370232+XZeipher@users.noreply.github.com>
2024-07-28 15:17:46 +0530

    Update test.py

--- Robot/modules/test.py
+++ Robot/modules/test.py
@@ -1,5 +1,5 @@
 from pyrogram import *
 
-@Client.on_message(filters.command("me", prefixes=["/","."]) && filters.me)
+@Client.on_message(filters.command("me", prefixes=["/","."]) & filters.me)
 async def test(ctk,msg):
     return await ctk.send_message(msg.chat.id,"nigga")
