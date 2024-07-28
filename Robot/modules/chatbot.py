b2190d371eedadfc810574906016f062f04da172
XZeipher <144370232+XZeipher@users.noreply.github.com>
2024-07-28 15:55:29 +0530

    Update chatbot.py

--- Robot/modules/chatbot.py
+++ Robot/modules/chatbot.py
@@ -1,17 +1,13 @@
 from PyCharacterAI import Client as Sex
 from pyrogram import *
 
-token = 'b7a5cf786e6a740638c1b46ad02b035c39d96943'
-auth = False
 
 @Client.on_message(filters.me & filters.text, group=5)
 async def chat(ctk,message):
-    global auth,token
     client = Sex()
+    token = 'b7a5cf786e6a740638c1b46ad02b035c39d96943'
     if message.text.lower().startswith("atri"):
-        if not auth:
-            await client.authenticate_with_token(token)
-            auth = True
+        await client.authenticate_with_token(token)
         character_id = "Km_81EXBw_-QSRq1JcAut7jqYUfGhLNcUVO9Ms5xYwQ"
         chat = await client.create_or_continue_chat(character_id)
         msg = f"Rishav: {message.text}"
