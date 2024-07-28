17525ad65facd83f5c52093046a221ca40519775
XZeipher <144370232+XZeipher@users.noreply.github.com>
2024-07-28 11:06:47 +0530

    Update echo.py

--- Robot/modules/echo.py
+++ Robot/modules/echo.py
@@ -1,7 +1,7 @@
-from Robot import bot
+
 from pyrogram import *
 
-@bot.on_message(filters.me)
+@Client.on_message(filters.me)
 async def echo(client,message):
     if message.text.startswith("echo"):
         text = message.text.split("echo")[1].strip()
