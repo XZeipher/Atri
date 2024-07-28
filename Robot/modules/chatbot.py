8981101a9398f475344ae3e6a4ea69d4bc854e6e
XZeipher <144370232+XZeipher@users.noreply.github.com>
2024-07-28 11:30:44 +0530

    Update chatbot.py

--- Robot/modules/chatbot.py
+++ Robot/modules/chatbot.py
@@ -3,11 +3,14 @@ from pyrogram import *
 
 client = Sex()
 token = 'b7a5cf786e6a740638c1b46ad02b035c39d96943'
+auth = False
 
 @Client.on_message(filters.me & filters.text)
 async def chat(ctk,message):
     if message.text.lower.startswith("atri"):
-        await client.authenticate_with_token(token)
+        if not auth:
+            await client.authenticate_with_token(token)
+            auth = True
         character_id = "Km_81EXBw_-QSRq1JcAut7jqYUfGhLNcUVO9Ms5xYwQ"
         chat = await client.create_or_continue_chat(character_id)
         message = f"Rishav: {message.text}"
